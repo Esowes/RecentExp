0752ee253f09f780e0d647343ccbc8f0b7ca556e
Serge Ostrowsky <esowes@gmail.com>
2020-11-20 15:00:35 +0100

    Dates of future events is displayed in RED

--- RecentExp/EventRow.swift
+++ RecentExp/EventRow.swift
@@ -32,81 +32,80 @@ struct EventRow: View {
     }
     
     var body: some View {
-        VStack {
-        HStack (alignment: .center)
-        {
-            if event.isSimulator
-            {
-             Image(systemName: "s.circle")
-                .font(.callout)
-                .foregroundColor(Color(UIColor.systemRed))
-            } else
-            {
-             Image(systemName: "airplane")
-                .font(.callout)
-                .foregroundColor(Color(UIColor.systemBlue))
-             }
-            Spacer(minLength: 2.0)
-        if UserDefaults.standard.bool(forKey: kairportNameDisplayed) && UserDefaults.standard.bool(forKey: kflightNumberDisplayed) {
             VStack {
-                Text(event.airportName ?? "")
+            HStack (alignment: .center)
+            {
+                if event.isSimulator
+                {
+                 Image(systemName: "s.circle")
+                    .font(.callout)
+                    .foregroundColor(Color(UIColor.systemRed))
+                } else
+                {
+                 Image(systemName: "airplane")
+                    .font(.callout)
+                    .foregroundColor(Color(UIColor.systemBlue))
+                 }
+                Spacer(minLength: 2.0)
+            if UserDefaults.standard.bool(forKey: kairportNameDisplayed) && UserDefaults.standard.bool(forKey: kflightNumberDisplayed) {
+                VStack {
+                    Text(event.airportName ?? "")
+                        .font(.headline)
+                        .modifier(textLayout())
+                    Spacer()
+                    Text(event.flightNumber ?? "")
+                        .font(.body)
+                        .modifier(textLayout())
+                        }
+                .padding(EdgeInsets(top: 0, leading: -10, bottom: 0, trailing: 0))
+
+            } else if UserDefaults.standard.bool(forKey: kairportNameDisplayed) {
+                    Text(event.airportName ?? "")
                     .font(.headline)
                     .modifier(textLayout())
-                Spacer()
-                Text(event.flightNumber ?? "")
+                    .padding(EdgeInsets(top: 0, leading: -10, bottom: 0, trailing: 0))
+
+                } else if UserDefaults.standard.bool(forKey: kflightNumberDisplayed) {
+                    Text(event.flightNumber ?? "")
                     .font(.body)
                     .modifier(textLayout())
-                    }
-            .padding(EdgeInsets(top: 0, leading: -10, bottom: 0, trailing: 0))
-
-        } else if UserDefaults.standard.bool(forKey: kairportNameDisplayed) {
-                Text(event.airportName ?? "")
-                .font(.headline)
-                .modifier(textLayout())
-                .padding(EdgeInsets(top: 0, leading: -10, bottom: 0, trailing: 0))
-
-            } else if UserDefaults.standard.bool(forKey: kflightNumberDisplayed) {
-                Text(event.flightNumber ?? "")
-                .font(.body)
-                .modifier(textLayout())
-                .padding(EdgeInsets(top: 0, leading: -10, bottom: 0, trailing: 0))
-            }
-            Spacer(minLength: 2.0)
-            if UserDefaults.standard.bool(forKey: kbiQualif) {
-                VStack {
+                    .padding(EdgeInsets(top: 0, leading: -10, bottom: 0, trailing: 0))
+                }
+                Spacer(minLength: 2.0)
+                if UserDefaults.standard.bool(forKey: kbiQualif) {
+                    VStack {
+                        Text(self.dateFormatter.string(from: event.eventDate ?? Date()) )
+                        .font(.callout) // .footnote is slightly smaller
+                            .foregroundColor(event.eventDate ?? Date() > Date() ? Color(UIColor.systemRed) : Color(UIColor.black))
+                        .modifier(textLayout())
+                        Spacer()
+                        if event.aircraftType == 1 { //330 ou 777
+                            if UserDefaults.standard.integer(forKey: kdualTypeSelection) == 0
+                            { Text("330").font(.footnote).modifier(textLayout()) }
+                                else if UserDefaults.standard.integer(forKey: kdualTypeSelection) == 1
+                            { Text("777").font(.footnote).modifier(textLayout()) }
+                                                   }
+                        else if event.aircraftType == 2 { //350 ou 787
+                            if UserDefaults.standard.integer(forKey: kdualTypeSelection) == 0
+                            { Text("350").font(.footnote).modifier(textLayout()) }
+                                else if UserDefaults.standard.integer(forKey: kdualTypeSelection) == 1
+                            {Text("787").font(.footnote).modifier(textLayout()) }
+                                }
+                    } // end of Vstack
+                    .padding(EdgeInsets(top: 0, leading: -10, bottom: 0, trailing: 0))
+                } else // NOT biQualif
+                {
                     Text(self.dateFormatter.string(from: event.eventDate ?? Date()) )
-                    .font(.body)
+                    .font(.callout) // .footnote is slightly smaller
+                        .foregroundColor(event.eventDate ?? Date() > Date() ? Color(UIColor.systemRed) : Color(UIColor.black))
                     .modifier(textLayout())
-                    Spacer()
-                    if event.aircraftType == 1 { //330 ou 777
-                        if UserDefaults.standard.integer(forKey: kdualTypeSelection) == 0
-                        { Text("330").font(.footnote).modifier(textLayout()) }
-                            else if UserDefaults.standard.integer(forKey: kdualTypeSelection) == 1
-                        { Text("777").font(.footnote).modifier(textLayout()) }
-                                               }
-                    else if event.aircraftType == 2 { //350 ou 787
-                        if UserDefaults.standard.integer(forKey: kdualTypeSelection) == 0
-                        { Text("350").font(.footnote).modifier(textLayout()) }
-                            else if UserDefaults.standard.integer(forKey: kdualTypeSelection) == 1
-                        {Text("787").font(.footnote).modifier(textLayout()) }
-                            }
-                } // end of Vstack
-                .padding(EdgeInsets(top: 0, leading: -10, bottom: 0, trailing: 0))
-            } else // NOT biQualif
-            {
-                Text(self.dateFormatter.string(from: event.eventDate ?? Date()) )
-                .font(.footnote) // .callout is slightly larger
-                .modifier(textLayout())
-                .padding(EdgeInsets(top: 0, leading: -10, bottom: 0, trailing: 0))
-            }
-        } // END of HStack
-            .frame(maxWidth: 200)
-            .padding(EdgeInsets(top: 0, leading: -10, bottom: 0, trailing: -10))
-        } // End of VStack
+                    .padding(EdgeInsets(top: 0, leading: -10, bottom: 0, trailing: 0))
+                }
+            } // END of HStack
+                .frame(maxWidth: 200)
+                .padding(EdgeInsets(top: 0, leading: -10, bottom: 0, trailing: -10))
+            } // End of VStack
             .frame(maxWidth: .infinity) // This is to ensure centering on larger displays
-        
-
- 
     } // END of body
 }
 
